59f0aa3f551967de698f228ede00d4243a6c6e37
s1maxx <maksimka024EG>
2022-03-12 18:37:43 +0200

    change dependencies

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
 import router from "./src/router/index.js"
 import errorMiddleware from './src/middlewares/error-middleware.js';
 
@@ -8,7 +7,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
 app.use(cors());
 app.use('/api', router);
 app.use(errorMiddleware);
