a37de79385dd52ab18940fb77cbc6e4fd65b942d
s1maxx <maksimka024EG>
2022-03-16 19:49:23 +0200

    fix bug after push changes

--- src/router/routes/profile.settings.routes.js
+++ src/router/routes/profile.settings.routes.js
@@ -5,17 +5,17 @@ import {admin, mirror_owner} from "../../db/roles.js";
 import {profile_settings} from "../../db/tables.js";
 
 const profileSettingsArrayGet = [
-    {path:'/profile/settings/:id', requiredRoles:[admin,mirror_owner], table:profile_settings, needAuth:true, function:profileSettingsController.getProfileSettings, description: "return profile settings by ID"},
-    {path:'/profile/settings-array/:id', requiredRoles:[admin,mirror_owner], table:profile_settings, needAuth:true, function:profileSettingsController.getProfileSettingsArray, description: "return profile settings array by ID"},
+    {path:'/profile-settings/:id', requiredRoles:[admin,mirror_owner], table:profile_settings, needAuth:true, function:profileSettingsController.getProfileSettings, description: "return profile settings by ID"},
+    {path:'/profile-settings-array/:id', requiredRoles:[admin,mirror_owner], table:profile_settings, needAuth:true, function:profileSettingsController.getProfileSettingsArray, description: "return profile settings array by ID"},
 ]
 
 const profileSettingsArrayPost = [
-    {path:'/profile/settings/add', validate: [
+    {path:'/profile-settings/add', validate: [
             body('name', 'Invalid name'),
             body('profile_id', 'Invalid profile id').notEmpty().isNumeric(),
             body('enable', 'Invalid boolean').isBoolean()
         ], needAuth:true, requiredRoles:[admin, mirror_owner], table:profile_settings, function:profileSettingsController.addProfileSettings, description: "add profile settings to DB"},
-    {path:'/profile/settings/add', validate: [
+    {path:'/profile-settings/add', validate: [
         body().isArray,
             body('*.name', 'Invalid name'),
             body('*.profile_id', 'Invalid profile id').notEmpty().isNumeric(),
@@ -24,16 +24,16 @@ const profileSettingsArrayPost = [
 ]
 
 const profileSettingsArrayDelete = [
-    {path:'/profile/settings/:id', needAuth:true, requiredRoles:[admin, mirror_owner], table:profile_settings, function:profileSettingsController.removeProfileSettings, description: "remove profile settings from DB"},
+    {path:'/profile-settings/:id', needAuth:true, requiredRoles:[admin, mirror_owner], table:profile_settings, function:profileSettingsController.removeProfileSettings, description: "remove profile settings from DB"},
 ]
 
 const profileSettingsArrayUpdate = [
-    {path:'/profile/settings/:id', validate: [
+    {path:'/profile-settings/:id', validate: [
             body('name', 'Invalid name'),
             body('profile_id', 'Invalid profile id').notEmpty().isNumeric(),
             body('enable', 'Invalid boolean').isBoolean()
         ], needAuth:true, requiredRoles:[admin, mirror_owner], table:profile_settings, function:profileSettingsController.updateProfileSettings, description: "update profile settings from DB"},
-    {path:'/profile/settings', validate: [
+    {path:'/profile-settings/upd', validate: [
             body().isArray,
             body('*.id', "Invalid id").notEmpty().isNumeric(),
             body('*.name', 'Invalid name'),
